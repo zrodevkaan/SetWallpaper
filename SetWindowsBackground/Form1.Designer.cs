0df6e02ebbd15e75dbf6bbb502138c6698661440
zrodevkaan <90235641+zrodevkaan@users.noreply.github.com>
2024-01-27 13:29:29 -0600

    File Dialog Added

--- SetWindowsBackground/Form1.Designer.cs
+++ SetWindowsBackground/Form1.Designer.cs
@@ -35,6 +35,8 @@ namespace SetWindowsBackground
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -55,11 +57,22 @@ namespace SetWindowsBackground
             this.textBox1.TabIndex = 1;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(132, 110);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 27);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Select File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(361, 165);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -69,6 +82,9 @@ namespace SetWindowsBackground
             this.ResumeLayout(false);
             this.PerformLayout();
         }
+
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button2;
 
         #endregion
 
--- SetWindowsBackground/Form1.cs
+++ SetWindowsBackground/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,14 @@ namespace SetWindowsBackground
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var FileDialog = new OpenFileDialog();
+            FileDialog.ShowDialog();
+            
+            DisplayPicture(FileDialog.FileName);
         }
     }
 }
