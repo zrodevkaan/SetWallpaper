7544ff14648e7233aa50f36a42a67dd8ba26ac5c
zrodevkaan <90235641+zrodevkaan@users.noreply.github.com>
2024-01-31 21:55:08 -0600

    added theme override

--- SetWindowsBackground/Form1.Designer.cs
+++ SetWindowsBackground/Form1.Designer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace SetWindowsBackground
 {
@@ -37,11 +38,13 @@ namespace SetWindowsBackground
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
             this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(132, 52);
+            this.button1.Location = new System.Drawing.Point(132, 38);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(82, 23);
             this.button1.TabIndex = 0;
@@ -51,7 +54,7 @@ namespace SetWindowsBackground
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 26);
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(337, 20);
             this.textBox1.TabIndex = 1;
@@ -59,7 +62,7 @@ namespace SetWindowsBackground
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(132, 110);
+            this.button2.Location = new System.Drawing.Point(132, 67);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(82, 27);
             this.button2.TabIndex = 2;
@@ -67,21 +70,47 @@ namespace SetWindowsBackground
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 100);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(337, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(118, 127);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 26);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Override Theme";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(361, 165);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Location = new System.Drawing.Point(15, 15);
             this.Name = "Form1";
-            this.Text = "Set Wallpaper";
             this.ResumeLayout(false);
             this.PerformLayout();
         }
+
+        private System.Windows.Forms.Button button3;
+
+        private System.Windows.Forms.ComboBox comboBox1;
 
         private System.Windows.Forms.ColorDialog colorDialog1;
         private System.Windows.Forms.Button button2;
@@ -104,7 +133,7 @@ namespace SetWindowsBackground
         private const uint SPI_SETDESKWALLPAPER = 0x14;
         private const uint SPIF_UPDATEINIFILE = 0x1;
         private const uint SPIF_SENDWININICHANGE = 0x2;
-
+        
         private static void DisplayPicture(string file_name)
         {
             uint flags = 0;
--- SetWindowsBackground/Form1.cs
+++ SetWindowsBackground/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,11 @@ namespace SetWindowsBackground
         public Form1()
         {
             InitializeComponent();
+            string[] themeFiles = Directory.GetFiles("C:\\Windows\\Resources\\Themes");
+            foreach (string file in themeFiles)
+            {
+                comboBox1.Items.Add(file);
+            }
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -29,6 +35,16 @@ namespace SetWindowsBackground
             FileDialog.ShowDialog();
             
             DisplayPicture(FileDialog.FileName);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            Process.Start(comboBox1.Text);
         }
     }
 }
